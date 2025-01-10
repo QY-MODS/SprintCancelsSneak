534e66ecfc817c070ad269ccacb19347e5385738
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2025-01-10 20:45:25 +0100

    cleanup and button settings update

--- include/Hooks.h
+++ include/Hooks.h
@@ -1,35 +1,11 @@
 #pragma once
-#include <shared_mutex>
-
-
-struct AnimationEventSink final : RE::BSTEventSink<RE::BSAnimationGraphEvent> {
-	AnimationEventSink() = default;
-    RE::BSEventNotifyControl ProcessEvent(const RE::BSAnimationGraphEvent* a_event,
-                                          RE::BSTEventSource<RE::BSAnimationGraphEvent>*) override;
-
-};
-
 
 namespace Hooks {
 
 	void Install();
 
-	inline std::atomic<bool> block_sprint_inputs = false;
-
-    inline bool is_classic_sprinting_redone_installed = false;
-    inline AnimationEventSink* player_animsink = nullptr;
-
 	inline std::map<RE::INPUT_DEVICE, RE::ButtonEvent*> sneak_events;
-	inline std::optional<std::chrono::high_resolution_clock::time_point> move_start_time;
 	inline std::optional<std::chrono::high_resolution_clock::time_point> sprint_held_start_time;
-	inline std::shared_mutex move_mutex;
-
-	struct InputHook {
-		static void thunk(RE::BSTEventSource<RE::InputEvent*>* a_dispatcher, RE::InputEvent* const* a_event);
-		static inline REL::Relocation<decltype(thunk)> func;
-
-		static bool ProcessInput(RE::InputEvent* event);
-	};
 
 	template <typename Handler>
     class PlayerInputHandlerHook : public Handler {
@@ -41,19 +17,14 @@ namespace Hooks {
     };
 
     RE::ButtonEvent* CreateSneakEvent(RE::INPUT_DEVICE a_device);
-    RE::ButtonEvent* CreateSprintEvent(RE::INPUT_DEVICE a_device);
+    void UpdateSneakEvents();
 
+    class ControlsChangedHook : public RE::Journal_SystemTab {
+        using ProcessMessage_t = RE::BSEventNotifyControl(RE::Journal_SystemTab::*)(const RE::BSGamerProfileEvent*, RE::BSTEventSource<RE::BSGamerProfileEvent>*);
+        static inline REL::Relocation<ProcessMessage_t> ProcessEvent;
+        RE::BSEventNotifyControl ProcessEvent_Hook(const RE::BSGamerProfileEvent* a_event, RE::BSTEventSource<RE::BSGamerProfileEvent>* a_eventSource);
+    public:
+        static void InstallHook(const REL::VariantID& varID);
+    };
 
-	class MovementHandlerHook:
-    public PlayerInputHandlerHook<RE::MovementHandler>
-    {
-		using ProcessThumbstick_t = decltype(&RE::MovementHandler::ProcessThumbstick);
-		static inline REL::Relocation<ProcessThumbstick_t> ProcessThumbstick;
-		void ProcessThumbstick_Hook(RE::ThumbstickEvent* a_event, RE::PlayerControlsData* a_data);
-	public:
-		static void InstallHook(const REL::VariantID& varID=RE::VTABLE_MovementHandler[0]);
-        static void SetIsMoving(bool is_moving=true);
-		static bool GetIsMoving();
-        static float GetMoveDuration();
-	};
 };
--- src/plugin.cpp
+++ src/plugin.cpp
@@ -4,22 +4,11 @@
 void OnMessage(SKSE::MessagingInterface::Message* message) {
     if (message->type == SKSE::MessagingInterface::kDataLoaded) {
         // Start
-
-        Hooks::player_animsink = new AnimationEventSink();
-
         Hooks::sneak_events[RE::INPUT_DEVICE::kKeyboard] = Hooks::CreateSneakEvent(RE::INPUT_DEVICE::kKeyboard);
         Hooks::sneak_events[RE::INPUT_DEVICE::kMouse] = Hooks::CreateSneakEvent(RE::INPUT_DEVICE::kMouse);
         Hooks::sneak_events[RE::INPUT_DEVICE::kGamepad] = Hooks::CreateSneakEvent(RE::INPUT_DEVICE::kGamepad);
     }
-    if (message->type == SKSE::MessagingInterface::kNewGame || message->type == SKSE::MessagingInterface::kPostLoadGame) {
-        // Post-load
-        if (RE::PlayerCharacter::GetSingleton()->AddAnimationGraphEventSink(Hooks::player_animsink)) {
-			logger::info("AnimationGraphEventSink added to PlayerCharacter");
-		}
-		/*else {
-			logger::error("Failed to add AnimationGraphEventSink to PlayerCharacter");
-        }*/
-    }
+
 }
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
 
